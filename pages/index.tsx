9c0983b1059b0bb4c564de89c474728005060b55
Muhammad Husnain <mlhlk1212@gmail.com>
2022-08-07 20:00:41 +0500

    feat: pushing

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,8 @@
 import { Flex } from '@chakra-ui/react';
 import type { NextPage } from 'next';
-import Head from 'next/head';
-import Image from 'next/image';
-import styles from '../styles/Home.module.css';
 
 const Home: NextPage = () => {
-  return (
-    <Flex>Hello</Flex>
-  );
+  return <Flex>Hello</Flex>;
 };
 
 export default Home;
